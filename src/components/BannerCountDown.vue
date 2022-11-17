01637a97d052cb123581d27c44cca43e9a7e52c3
reistence <gvlamberti@gmail.com>
2022-11-17 18:25:40 +0100

    countdown functioning

--- src/components/BannerCountDown.vue
+++ src/components/BannerCountDown.vue
@@ -1,6 +1,31 @@
 <script>
 export default {
   name: "BannerCountDown",
+  data() {
+    return {
+      days: null,
+      hours: null,
+      minutes: null,
+      seconds: null,
+    };
+  },
+  created() {
+    setInterval(this.updateCount, 1000);
+  },
+  computed: {},
+  methods: {
+    updateCount() {
+      const thisYear = new Date().getFullYear();
+      const target = new Date(`January 01 ${thisYear + 11} 00:00:00`);
+
+      const today = new Date();
+      const diff = target - today;
+      this.days = Math.floor(diff / 1000 / 60 / 60 / 24);
+      this.hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+      this.minutes = Math.floor(diff / 1000 / 60) % 60;
+      this.seconds = Math.floor(diff / 1000) % 60;
+    },
+  },
 };
 </script>
 
@@ -9,27 +34,30 @@ export default {
     <div class="wrapper">
       <div class="countdown">
         <div class="days">
-          <p>3399</p>
+          <p v-text="this.days"></p>
           <p>days</p>
         </div>
-        <div class="hours">
-          <p>17</p>
+        <div>
+          <p v-text="this.hours < 10 ? '0' + this.hours : this.hours"></p>
           <p>hours</p>
         </div>
-        <div class="mins">
-          <p>28</p>
+        <div>
+          <p v-text="this.minutes < 10 ? '0' + this.minutes : this.minutes"></p>
           <p>mins</p>
         </div>
-        <div class="secs">
-          <p>17</p>
-          <p>secs</p>
+        <div id="secs">
+          <p
+            id="secsN"
+            v-text="this.seconds < 10 ? '0' + this.seconds : this.seconds"
+          ></p>
+          <p id="secsP">secs</p>
         </div>
       </div>
       <div class="banner-txt">
         <p>subscribe for updates</p>
         <div>
-          <input type="text" placeholder="Please fill your email" />
-          <button>Subscribe</button>
+          <input type="email" placeholder="Please fill your email" />
+          <button @click="updateCount">Subscribe</button>
         </div>
       </div>
     </div>
@@ -58,24 +86,45 @@ export default {
       display: flex;
       flex-direction: row;
       justify-content: center;
-      align-items: center;
+      align-items: stretch;
+
       gap: 1em;
 
       div {
         background-color: rgba(0, 0, 0, 0.77);
         padding: 1.5em;
+        width: max-content;
         margin-bottom: 1em;
         border-radius: 5px;
         color: $white;
         text-align: center;
+        position: relative;
 
         p:first-child {
           font-size: 4rem;
+          transition: all 300ms linear;
         }
         p:nth-child(2) {
           text-transform: uppercase;
           font-size: 1rem;
         }
+
+        // #secs {
+        //   width: 10em;
+        // }
+
+        // #secsN {
+        //   position: absolute !important;
+        //   top: 45%;
+        //   left: 50%;
+        //   transform: translate(-50%, -50%);
+        // }
+        // #secsP {
+        //   position: absolute;
+        //   bottom: 15%;
+        //   left: 50%;
+        //   transform: translate(-50%, -50%);
+        // }
       }
     }
 
