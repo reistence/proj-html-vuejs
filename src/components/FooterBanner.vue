39de8c5e681557f5ffb139b81731ee0a04c92ad7
reistence <gvlamberti@gmail.com>
2022-11-18 12:21:19 +0100

    debug  borders removed, footerbanner bg changed

--- src/components/FooterBanner.vue
+++ src/components/FooterBanner.vue
@@ -23,7 +23,7 @@ export default {
 <style lang="scss" scoped>
 @use "../styles/partials/variables" as *;
 .container-fluid {
-  background-color: $black;
+  background-color: black !important;
 }
 
 .container {
